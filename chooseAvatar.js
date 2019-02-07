7b22e07217ae098694af7f389d316935b488cec2
Geoffrey04 <43751762+Geoffrey04@users.noreply.github.com>
2019-02-07 11:24:53 +0100

    modify chooseAvatar.js ( img problem )

--- chooseAvatar.js
+++ chooseAvatar.js
@@ -6,24 +6,24 @@ var index =0;
 document.getElementById('value1').addEventListener("click", function () {
 
     index = document.getElementById('value1').value;
-   window.location.href= "game.php?index="+index;
+   window.location.href= "vu/game.php?index="+index;
 
 });
 document.getElementById('value2').addEventListener("click", function () {
 
     index = document.getElementById('value2').value;
-    window.location.href = "game.php?index="+index;
+    window.location.href = "vu/game.php?index="+index;
 
 });
 document.getElementById('value3').addEventListener("click", function () {
 
     index = document.getElementById('value3').value;
-    window.location.href = "game.php?index="+index;
+    window.location.href = "vu/game.php?index="+index;
 
 });
 document.getElementById('value4').addEventListener("click", function () {
 
     index = document.getElementById('value4').value;
-    window.location.href = "game.php?index="+index;
+    window.location.href = "vu/game.php?index="+index;
 
 });
