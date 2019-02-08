f61f22f4c3ec6c75fed9c43538846260c484d5c4
cyriakleclercq <cyriakleclercq@gmail.com>
2019-02-08 11:16:56 +0100

    test start

--- model/check_log.php
+++ model/check_log.php
@@ -50,7 +50,7 @@ class check_log
             $_SESSION['skin'] = $skin;
             $_SESSION['id'] = $id;
 
-            header('../Location:vu/perso.php');
+            header('../Location:start.php');
 
         }
 
--- vu/affiche.php
+++ vu/affiche.php
@@ -5,3 +5,10 @@
  * Date: 07/02/2019
  * Time: 20:04
  */
+
+require "../controleur/affichage.php";
+require "../model/affichage.php";
+
+$ctrl = new affichage_controleur();
+
+$ctrl->UserCreate();
